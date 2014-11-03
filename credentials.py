c4e6d92966014ff5f96a691e7b7402827843b3fd
Andrew Briggs <theandrewbriggs@gmail.com>
2014-11-02 20:45:23 -0600

    Includes template for credentials file

---
+++ credentials.py
@@ -0,0 +1,14 @@
+# When you register your app with dev.twitter.com, you'll
+# need to generate an access token. Fill in the following
+# strings with the correct info below.
+#
+# DO NOT SHARE THIS INFORMATION.
+# It should remain private.
+# That includes uploading it to Github. (Don't do it.)
+
+access = {
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token_key': '',
+    'access_token_secret': '',
+}
